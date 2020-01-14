a5357a01e6af9e9dec721aca30061266c4e4438e
androba <andresrarias@gmail.com>
2020-01-14 17:53:07 -0300

    .

--- module_auto_update/models/auto_update.py
+++ module_auto_update/models/auto_update.py
@@ -1,7 +1,11 @@
 # -*- coding: utf-8 -*-
 
 import git
+import logging
+import subprocess
 from odoo import api, models
+
+_logger = logging.getLogger(__name__)
 
 
 class AutoUpdate(models.TransientModel):
@@ -15,3 +19,23 @@ class AutoUpdate(models.TransientModel):
             res = g.pull()
             print('-------------res git ---------------')
             print(res)
+            # NOTE: update /etc/.odooconf replaces all content
+            # TODO: replace test_update,module_auto_update with variable value
+            f = open("/etc/.odooconf", "w")
+            f.write("ARG1=--update test_update,module_auto_update")
+            f.close()
+
+            # NOTE: restart server
+            # TODO: run the service as odoo user without the fucking password
+
+            # import pdb; pdb.set_trace()
+            process = subprocess.Popen('whoami', stdout=subprocess.PIPE)
+            stdout = process.communicate()[0]
+            _logger.info('--------------WHOAMI----------------------')
+            _logger.info('STDOUT:{}'.format(stdout))
+            print('STDOUT:{}'.format(stdout))
+
+            print('----------------------')
+            print(res)
+            command = "sudo systemctl restart odooupdate"
+            subprocess.run(command, shell=True)
